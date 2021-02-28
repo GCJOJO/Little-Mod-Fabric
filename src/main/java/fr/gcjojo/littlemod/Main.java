4304e4745e2997855aabcf38c96625c254ad3ae9
GCJOJO <39382270+GCJOJO@users.noreply.github.com>
2021-02-28 11:57:12 +0100

    Added a Custom Block

--- src/main/java/fr/gcjojo/littlemod/Main.java
+++ src/main/java/fr/gcjojo/littlemod/Main.java
@@ -1,6 +1,12 @@
 package fr.gcjojo.littlemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.registry.Registry;
@@ -9,6 +15,8 @@ import net.minecraft.util.Identifier;
 public class Main implements ModInitializer{
 
     public static final Item  FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+    public static final Block FABRIC_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+
 
     @Override
     public void onInitialize() {
@@ -17,6 +25,8 @@ public class Main implements ModInitializer{
         // Proceed with mild caution.
 
         Registry.register(Registry.ITEM, new Identifier("lm", "fabric_item"), FABRIC_ITEM);
+        Registry.register(Registry.BLOCK, new Identifier("lm", "fabric_block"), FABRIC_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("lm", "fabric_block"), new BlockItem(FABRIC_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 
         System.out.println("Little Fabric mod initialized !");
     }
